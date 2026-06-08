8ab1c6651d9f82b80d2c819f2abce5934a0dc5f3
sindhukumarray <sindhukumarray007@gmail.com>
2026-06-08 06:21:40 +0530

    Added dark mode and dashboard improvements

--- public/script.js
+++ public/script.js
@@ -245,3 +245,28 @@ if(searchInput){
     });
 
 }
+//dark mode 
+const themeBtn =
+document.getElementById("themeBtn");
+
+themeBtn.addEventListener("click",()=>{
+
+    document.body.classList.toggle("dark-mode");
+
+    if(
+        document.body.classList.contains("dark-mode")
+    ){
+
+        themeBtn.innerText =
+        "☀️ Light Mode";
+
+    }
+
+    else{
+
+        themeBtn.innerText =
+        "🌙 Dark Mode";
+
+    }
+
+});
