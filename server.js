f93e2adadccfccac166bd900a52ae359a3d09c89
sindhukumarray <sindhukumarray007@gmail.com>
2026-06-08 08:27:50 +0530

    Configured Railway MySQL database

--- server.js
+++ server.js
@@ -9,10 +9,11 @@ app.use(express.json());
 app.use(express.static("public"));
 
 const db = mysql.createConnection({
-    host: "localhost",
-    user: "root",
-    password: "ersindhu",
-    database: "studentdb"
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  port: process.env.DB_PORT
 });
 
 // Database Connection
