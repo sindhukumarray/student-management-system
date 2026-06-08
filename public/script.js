0f4be22a1b8737b53bbef3a9f865cc3250e3e478
sindhukumarray <sindhukumarray007@gmail.com>
2026-06-08 05:42:24 +0530

    Initial DBMS Student Management System

---
+++ public/script.js
@@ -0,0 +1,247 @@
+const form = document.getElementById("studentForm");
+const list = document.getElementById("studentList");
+
+let editId = null;
+
+loadStudents();
+
+// Date
+
+const dateElement = document.getElementById("date");
+
+if(dateElement){
+    dateElement.innerText =
+    "Date : " + new Date().toLocaleDateString();
+}
+
+// Submit Form
+
+form.addEventListener("submit", async (e) => {
+
+    e.preventDefault();
+
+    const student = {
+
+        name: document.getElementById("name").value.trim(),
+        email: document.getElementById("email").value.trim(),
+        course: document.getElementById("course").value
+
+    };
+
+    // Validation
+
+    if(student.name.length < 3){
+
+        alert("Name must be at least 3 characters");
+        return;
+
+    }
+
+    if(!student.email.includes("@")){
+
+        alert("Please enter valid email");
+        return;
+
+    }
+
+    if(student.course === ""){
+
+        alert("Please select course");
+        return;
+
+    }
+
+    // UPDATE
+
+    if(editId){
+
+        await fetch(`/update/${editId}`,{
+
+            method:"PUT",
+
+            headers:{
+                "Content-Type":"application/json"
+            },
+
+            body:JSON.stringify(student)
+
+        });
+
+        alert("Student Updated Successfully");
+
+        editId = null;
+
+        document.querySelector("button[type='submit']")
+        .innerText = "Add Student";
+
+    }
+
+    // ADD
+
+    else{
+
+        await fetch("/add",{
+
+            method:"POST",
+
+            headers:{
+                "Content-Type":"application/json"
+            },
+
+            body:JSON.stringify(student)
+
+        });
+
+        alert("Student Added Successfully");
+
+    }
+
+    form.reset();
+
+    loadStudents();
+
+});
+
+// Load Students
+
+async function loadStudents(){
+
+    const res = await fetch("/students");
+
+    const data = await res.json();
+
+    document.getElementById("totalStudents").innerText =
+    data.length;
+
+     if(data.length === 0){
+
+        list.innerHTML = `
+            <div class="empty">
+                No Students Found
+            </div>
+        `;
+
+        return;
+    }
+
+    list.innerHTML = "";
+
+    data.forEach(student => {
+
+        list.innerHTML += `
+
+        <div class="student">
+
+ <div>
+    <span class="id-badge">#${student.id}</span>
+
+    <h3>${student.name}</h3>
+
+    <p>${student.email}</p>
+
+    <p>${student.course}</p>
+</div>
+
+            <div class="btn-group">
+
+                <button
+                class="edit-btn"
+                onclick="editStudent(
+                ${student.id},
+                '${student.name}',
+                '${student.email}',
+                '${student.course}'
+                )">
+
+                Edit
+
+                </button>
+
+                <button
+                class="delete-btn"
+                onclick="deleteStudent(${student.id})">
+
+                Delete
+
+                </button>
+
+            </div>
+
+        </div>
+
+        `;
+
+    });
+
+}
+
+// Edit Student
+
+function editStudent(id,name,email,course){
+
+    document.getElementById("name").value = name;
+
+    document.getElementById("email").value = email;
+
+    document.getElementById("course").value = course;
+
+    editId = id;
+
+    document.querySelector("button[type='submit']")
+    .innerText = "Update Student";
+
+}
+
+// Delete Student
+
+async function deleteStudent(id){
+
+    const confirmDelete =
+    confirm("Are you sure you want to delete this student?");
+
+    if(!confirmDelete) return;
+
+    await fetch(`/delete/${id}`,{
+
+        method:"DELETE"
+
+    });
+
+    alert("Student Deleted Successfully");
+
+    loadStudents();
+
+}
+
+// Search
+
+const searchInput =
+document.getElementById("search");
+
+if(searchInput){
+
+    searchInput.addEventListener("keyup", function(){
+
+        const value =
+        this.value.toLowerCase();
+
+        const students =
+        document.querySelectorAll(".student");
+
+        students.forEach(student => {
+
+            student.style.display =
+
+            student.innerText
+            .toLowerCase()
+            .includes(value)
+
+            ? "flex"
+
+            : "none";
+
+        });
+
+    });
+
+}
---
+++ server.js
@@ -0,0 +1,130 @@
+const express = require("express");
+const mysql = require("mysql2");
+const cors = require("cors");
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+app.use(express.static("public"));
+
+const db = mysql.createConnection({
+    host: "localhost",
+    user: "root",
+    password: "ersindhu",
+    database: "studentdb"
+});
+
+// Database Connection
+
+db.connect((err) => {
+
+    if(err){
+
+        console.log(err);
+
+    }else{
+
+        console.log("Database Connected");
+
+    }
+
+});
+
+// ADD STUDENT
+
+app.post("/add", (req,res)=>{
+
+    const {name,email,course} = req.body;
+
+    db.query(
+
+        "INSERT INTO students(name,email,course) VALUES(?,?,?)",
+
+        [name,email,course],
+
+        (err,result)=>{
+
+            if(err) throw err;
+
+            res.send("Student Added");
+
+        }
+
+    );
+
+});
+
+// GET ALL STUDENTS
+
+app.get("/students",(req,res)=>{
+
+    db.query(
+
+        "SELECT * FROM students",
+
+        (err,result)=>{
+
+            if(err) throw err;
+
+            res.json(result);
+
+        }
+
+    );
+
+});
+
+// UPDATE STUDENT
+
+app.put("/update/:id",(req,res)=>{
+
+    const {name,email,course} = req.body;
+
+    db.query(
+
+        "UPDATE students SET name=?, email=?, course=? WHERE id=?",
+
+        [name,email,course,req.params.id],
+
+        (err,result)=>{
+
+            if(err) throw err;
+
+            res.send("Student Updated Successfully");
+
+        }
+
+    );
+
+});
+
+// DELETE STUDENT
+
+app.delete("/delete/:id",(req,res)=>{
+
+    db.query(
+
+        "DELETE FROM students WHERE id=?",
+
+        [req.params.id],
+
+        (err,result)=>{
+
+            if(err) throw err;
+
+            res.send("Student Deleted");
+
+        }
+
+    );
+
+});
+
+// SERVER
+
+app.listen(5000,()=>{
+
+    console.log("Server Running on Port 5000");
+
+});
